3586d63310d568deadfb1a68920450b7644f31eb
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-09-05 08:52:56 +0200

    Tag 3

--- src/main/java/de/lubowiecki/basics/uebung4/ProductApp.java
+++ src/main/java/de/lubowiecki/basics/uebung4/ProductApp.java
@@ -6,24 +6,35 @@ import java.util.Scanner;
 
 public class ProductApp {
 
+    // Scanner ermöglich die Eingabe von der Konsole
     private static final Scanner scanner = new Scanner(System.in);
+
+    // In der ArrayList werden die Produkte gesammelt
     private static final List<Produkt> produkte = new ArrayList<>();
 
+
+    // String für die Anzeige der AUswahlmöglichkeiten
     private static final String KEYS = "\n***************************************\n" +
                                         " N : Neues Produkt \n" +
                                         " L : Produkte auflisten \n" +
                                         " Q : Programm verlassen \n" +
                                         "***************************************\n";
+
     public static void main(String[] args) {
 
+        // Wird bis zum Abbruch mit q ausgeführt
         app: while(true) {
             System.out.println(KEYS);
 
             System.out.print("Was willst du machen? ");
+
+            // trim schneidet ungewünschte Zeichen an beiden Enden des Strings
+            // toLowerCase ändert alle Zeichen im String in Kleinbuchstaben
             String input = scanner.next().trim().toLowerCase();
 
             System.out.println();
 
+            // Auswahl des Benutzers wird verarbeitet
             switch(input) {
                 case "n" -> addProduct();
                 case "l" -> showProduct(); 
@@ -50,7 +61,7 @@ public class ProductApp {
     }
 
     private static Produkt createProduct() {
-        scanner.nextLine(); // fix
+        scanner.nextLine(); // clear fix
         System.out.println("Produkteingabe.");
         System.out.print("Name: ");
         var name = scanner.nextLine();
@@ -60,8 +71,6 @@ public class ProductApp {
         var menge = scanner.nextInt();
         System.out.print("Preis: ");
         var preis = scanner.nextDouble();
-
         return new Produkt(name, beschreibung, menge, preis);
-
     }
 }
